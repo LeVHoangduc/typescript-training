1a66260325b11cb4c4153cdaeee48f743dacb5a1
LeVHoangduc <levhoangduc@gmail.com>
2023-09-05 08:56:48 +0700

    feat:implement causes example

---
+++ src/classHeritageExamples/implementCauses.ts
@@ -0,0 +1,27 @@
+interface Pingable {
+  ping(): void;
+}
+
+class Sonar implements Pingable {
+  ping() {
+    console.log("ping!");
+  }
+}
+
+class Ball implements Pingable {
+  pong() {
+    console.log("pong!");
+  }
+}
+
+interface Checkable {
+  check(name: number): boolean;
+}
+
+class NameChecker implements Checkable {
+  check(s) {
+    // Parameter 's' implicitly has an 'any' type.
+    // Notice no error here
+    return s.toLowerCase() === "ok";
+  }
+}
