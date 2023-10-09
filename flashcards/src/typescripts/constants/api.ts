c15ae08d51946592ff54b0020cb2c73d9a78670e
LeVHoangduc <levhoangduc@gmail.com>
2023-10-09 10:53:37 +0700

    feat:update api base url

--- flashcards/src/typescripts/constants/api.ts
+++ flashcards/src/typescripts/constants/api.ts
@@ -1,7 +1,6 @@
 /**
  * API gateway url
  */
-
-const API_BASE_URL: string = 'https://learn-flashcards.onrender.com/'
+const API_BASE_URL: string = 'https://learn-flashcards.onrender.com'
 
 export default API_BASE_URL
